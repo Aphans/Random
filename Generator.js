942b5e38db63ed39ff4d8898692cd0af457ff817
Aphans <rapperanqeel@hotmail.com>
2022-11-25 09:16:56 +0100

    need only a filter

--- Generator.js
+++ Generator.js
@@ -1,12 +1,15 @@
 const jsonData = require('./Nombres.json');
 let jsonDataObj = JSON.parse(JSON.stringify(jsonData));
-
-if(jsonDataObj.length%2==0){
-    const aleatorio1 = jsonDataObj[Math.floor(Math.random() * jsonDataObj.length)];
-    const aleatorio2 = jsonDataObj[Math.floor(Math.random() * jsonDataObj.length)];
-    const filtrar = jsonDataObj.filter((el)=>jsonDataObj!==aleatorio1);
+let listado=[...jsonDataObj];
+if(listado.length%2==0){
+    while(listado.length>0){
+    const aleatorio1 = listado[Math.floor(Math.random() * listado.length)];
+    const aleatorio2 = listado[Math.floor(Math.random() * listado.length)];
+    if(!aleatorio1.includes(aleatorio2))
+    listado = listado.filter((el)=>el!=aleatorio1 && el!=aleatorio2);
     console.log(aleatorio1,aleatorio2);
-    console.log(jsonDataObj);
+    console.log(listado);
+    }
 }
 else{
     console.log("Intentálo de nuevo");
