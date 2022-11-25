6a3c9c19e706e545dc924acecc2e20e4a0ac6672
Aphans <rapperanqeel@hotmail.com>
2022-11-25 12:35:33 +0100

    100% Functional

--- Generator.js
+++ Generator.js
@@ -4,9 +4,9 @@ let listado=[...jsonDataObj];
 if(listado.length%2==0){
     while(listado.length>0){
     const aleatorio1 = listado[Math.floor(Math.random() * listado.length)];
+    listado = listado.filter((el)=>el!=aleatorio1);
     const aleatorio2 = listado[Math.floor(Math.random() * listado.length)];
-    if(!aleatorio1.includes(aleatorio2))
-    listado = listado.filter((el)=>el!=aleatorio1 && el!=aleatorio2);
+    listado = listado.filter((el)=>el!=aleatorio2);
     console.log(aleatorio1,aleatorio2);
     console.log(listado);
     }
